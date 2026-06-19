e2d6583cce9d43468523d6abd58aaf4110973e0f
kousic25 <kousice@gmail.com>
2026-06-19 14:36:03 +0530

    commit all

---
+++ while loop/traffic ligth.py
@@ -0,0 +1,13 @@
+import time
+
+lights = ["🔴 RED - Stop", "🟡 YELLOW - Slow", "🟢 GREEN - Go"]
+durations = [3, 1, 3]
+cycles = 2
+cycle = 0
+while cycle < cycles:
+    i = 0
+    while i < len(lights):
+        print(f"{lights[i]} ({durations[i]}s)")
+        time.sleep(durations[i])
+        i += 1
+    cycle += 1
